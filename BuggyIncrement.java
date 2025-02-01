75a357bc9bfe47de0296edf2f5c9d17edd8305a2
BugHunter-X <askzenai@gmail.com>
2025-02-01 07:34:03 +0100

    Add BuggyIncrement.java file

---
+++ BuggyIncrement.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    private int x = 0;
+
+    public void incrementX() {
+        x++; // This line might cause an unexpected behavior
+    }
+
+    public int getX() {
+        return x;
+    }
+}
